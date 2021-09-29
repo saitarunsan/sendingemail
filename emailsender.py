0e4ff4b567a91c51d3bafa46fe4c21265f16add5
sai tarun <saitarunsan@gmail.com>
2021-09-29 14:02:49 +0530

    final commit

--- emailsender.py
+++ emailsender.py
@@ -7,7 +7,7 @@ message='this mail is send my server using python'
 server = smtplib.SMTP('smtp.gmail.com',587)
 server.starttls()
 server.login(sendermail,password)
-print('login sucessful')#to login successful turn on the less secure app access
+print('login sucessful')#to login successful you must turn on the less secure app access in google.com
 
 server.sendmail(sendermail,receivermail,message)
 print('mail sent successsfully')
